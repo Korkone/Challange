3b7e9e00b7cace7d2307c1b3e7a793ba7c46b717
Pascal <pcden@web.de>
2024-03-16 17:05:26 +0100

    Updating Saving Method
    implemented the saving of the Data inbetween the steps themself so that if the programm crashes the steps before are saved and not lost

--- Telefonbuch.py
+++ Telefonbuch.py
@@ -16,6 +16,17 @@ def str_to_dict(string):
 
 Telefonbuch = str_to_dict(Data)
 
+def Save_to_File():
+    Speicherdatei = str(Telefonbuch)
+    try:
+        geeky_file = open('Telefonbuch.txt', 'w')
+        geeky_file.write(Speicherdatei.translate({ord("'"): None}))
+        geeky_file.close()
+
+
+    except:
+        print("Unable to write to file")
+
 def Name_input():
     global Name
 
@@ -51,20 +62,26 @@ def telefonbuchoption():
             Name_input()
             Number_input()
             Telefonbuch[Name] = Number
+            os.system('cls' if os.name == 'nt' else 'clear')
+            print(Name +" is now saved in the list with the Number: " + Number)
+            Save_to_File()
             continue
 
         elif choice == str(3):
             print("\n\n")
             Name_input()
+            os.system('cls' if os.name == 'nt' else 'clear')
             try:
                 del Telefonbuch[Name]
                 print(Name + " got deleted from the list")
             except KeyError:
                 print("This name is not on your list.")
+            Save_to_File()
             continue
 
         elif choice == str(2):
             print("\n\n")
+            os.system('cls' if os.name == 'nt' else 'clear')
             for key, value in Telefonbuch.items():
                 print(key, value)
             continue
@@ -79,11 +96,3 @@ def telefonbuchoption():
 telefonbuchoption()
 Speicherdatei = str(Telefonbuch)
 
-try:
-    geeky_file = open('Telefonbuch.txt', 'w')
-    geeky_file.write(Speicherdatei.translate({ord("'"): None}))
-    geeky_file.close()
-
-
-except:
-    print("Unable to write to file")
